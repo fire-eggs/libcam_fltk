ac7d48072bbe02145a6040ad41be45dc611ec456
Dustin Kerstein <dustin.kerstein@gmail.com>
2021-12-10 16:33:22 +0000

    Separate video code into own method

--- apps/libcamera_control.cpp
+++ apps/libcamera_control.cpp
@@ -49,53 +49,58 @@ static int get_key_or_signal(VideoOptions const *options, pollfd p[1])
 	return key;
 }
 
+static void Video(std::unique_ptr<Output> & output, int argc, char *argv[]) {
+	std::cerr << "DUSTIN START" << std::endl;
+	LibcameraEncoder app;
+	VideoOptions *options = app.GetOptions();
+	options->Parse(argc, argv);
+	app.SetEncodeOutputReadyCallback(std::bind(&Output::OutputReady, output.get(), _1, _2, _3, _4));
+	app.StartEncoder();
+	app.OpenCamera();
+	app.ConfigureVideo();
+	app.StartCamera();
+	auto start_time = std::chrono::high_resolution_clock::now();
+
+	// Monitoring for keypresses and signals.
+	signal(SIGUSR1, default_signal_handler);
+	signal(SIGUSR2, default_signal_handler);
+	pollfd p[1] = { { STDIN_FILENO, POLLIN, 0 } };
+
+	for (unsigned int count = 0; ; count++)
+	{
+		LibcameraEncoder::Msg msg = app.Wait();
+		if (msg.type == LibcameraEncoder::MsgType::Quit)
+			break;
+		else if (msg.type != LibcameraEncoder::MsgType::RequestComplete)
+			throw std::runtime_error("unrecognised message!");
+		int key = get_key_or_signal(options, p);
+		if (key == '\n')
+			output->Signal();
+		auto now = std::chrono::high_resolution_clock::now();
+		bool timeout = !options->frames && options->timeout &&
+					   (now - start_time > std::chrono::milliseconds(options->timeout));
+		bool frameout = options->frames && count >= options->frames;
+		if (timeout || frameout || key == 'x' || key == 'X')
+		{
+			app.StopCamera();
+			app.StopEncoder();
+			break;
+		}
+		CompletedRequestPtr &completed_request = std::get<CompletedRequestPtr>(msg.payload);
+		app.EncodeBuffer(completed_request, app.VideoStream());
+	}
+	output->WriteOut();
+	std::cerr << "DUSTIN END" << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
 	try
 	{
 		std::unique_ptr<Output> output = std::unique_ptr<Output>(Output::Create());
 		while (true) {
-			std::cerr << "DUSTIN START" << std::endl;
-			LibcameraEncoder app;
-			VideoOptions *options = app.GetOptions();
-			options->Parse(argc, argv);
-			app.SetEncodeOutputReadyCallback(std::bind(&Output::OutputReady, output.get(), _1, _2, _3, _4));
-			app.StartEncoder();
-			app.OpenCamera();
-			app.ConfigureVideo();
-			app.StartCamera();
-			auto start_time = std::chrono::high_resolution_clock::now();
-
-			// Monitoring for keypresses and signals.
-			signal(SIGUSR1, default_signal_handler);
-			signal(SIGUSR2, default_signal_handler);
-			pollfd p[1] = { { STDIN_FILENO, POLLIN, 0 } };
-
-			for (unsigned int count = 0; ; count++)
-			{
-				LibcameraEncoder::Msg msg = app.Wait();
-				if (msg.type == LibcameraEncoder::MsgType::Quit)
-					break;
-				else if (msg.type != LibcameraEncoder::MsgType::RequestComplete)
-					throw std::runtime_error("unrecognised message!");
-				int key = get_key_or_signal(options, p);
-				if (key == '\n')
-					output->Signal();
-				auto now = std::chrono::high_resolution_clock::now();
-				bool timeout = !options->frames && options->timeout &&
-							   (now - start_time > std::chrono::milliseconds(options->timeout));
-				bool frameout = options->frames && count >= options->frames;
-				if (timeout || frameout || key == 'x' || key == 'X')
-				{
-					app.StopCamera();
-					app.StopEncoder();
-					break;
-				}
-				CompletedRequestPtr &completed_request = std::get<CompletedRequestPtr>(msg.payload);
-				app.EncodeBuffer(completed_request, app.VideoStream());
-			}
-			output->WriteOut();
-			std::cerr << "DUSTIN END" << std::endl;
+			Video(output, argc, argv);
+			std::this_thread::sleep_for (std::chrono::milliseconds(1000));
 		}
 	}
 	catch (std::exception const &e)
