990c467cac1f6e0b4010ad9aae58a9d1215c04d6
fire-eggs <fire-eggs>
2022-04-18 11:18:53 -0400

    Insure /u/l/bin/applications folder exists

--- install/install.sh
+++ install/install.sh
@@ -3,5 +3,6 @@
 #
 mkdir -p /usr/local/share/libcam_fltk
 cp libcam_fltk.sh /usr/local/bin/libcam_fltk
+mkdir -p /usr/local/share/applications
 cp libcam_fltk.desktop /usr/local/share/applications
 cp * /usr/local/share/libcam_fltk
